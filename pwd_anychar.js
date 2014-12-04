dba5742827affe68d68cb1ffa890ffbfd8a682f6
Nathan <radmaps@yahoo.com>
2014-12-03 18:57:16 -0600

    Create pwd_anychar.js
    simple javascript function to use any single character to hide password input in an HTML element

---
+++ pwd_anychar.js
@@ -0,0 +1,31 @@
+   function Mod4Passwd(pinFldID,hpinFldID,theChar)
+   {  /* Overview:
+           simple alternative to <input type="password" ...
+         uses asteriks, but could be any character
+         Details:
+         transfers data from pinFld to hidden field hpinFld,
+         while making the characters Asteriks in the visible one
+          (works well with various input styles)
+           use with onkeyup= 
+           e.g.
+                  onkeyup="Mod4Passwd('pinFld','hpinFld','*');"
+          */
+     theChar = theChar.slice(0,1);
+     var taElem = document.getElementById(pinFldID);
+     var htaElem = document.getElementById(hpinFldID);
+     
+     if ((taElem != null) && (htaElem != null)) {
+       var theStr = taElem.value;
+       var inputStr = htaElem.value;
+       // first, make them the same length to accomodate backspace
+       inputStr = inputStr.substr(0,theStr.length);
+       for (i=0;i<theStr.length;i++)
+        if (theStr.charAt(i) != theChar)
+       { inputStr = inputStr.slice(0,i) + theStr.charAt(i) + 
+                     inputStr.slice(i+1);
+         theStr = theStr.slice(0,i) + theChar + theStr.slice(i+1);
+       }
+       taElem.value = theStr;
+       htaElem.value = inputStr;
+    }
+   }
